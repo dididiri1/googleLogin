3c4e8827b706d2300dd6e504e697ac7ff8385441
dididiri1 <dididiri1@naver.com>
2024-07-04 14:57:10 +0900

    feat: 구글 영수증 검증 권한 Oauth 로그인 기능 추가

--- src/main/java/com/example/googlelogin/controller/GoogleController.java
+++ src/main/java/com/example/googlelogin/controller/GoogleController.java
@@ -26,7 +26,6 @@ public class GoogleController {
     private String redirectUri;
 
     private final String tokenUri = "https://oauth2.googleapis.com/token";
-    private final String userInfoUri = "https://www.googleapis.com/oauth2/v2/userinfo";
 
     @GetMapping("/login")
     public String login(Model model) {
@@ -34,14 +33,14 @@ public class GoogleController {
                 "?client_id=" + clientId +
                 "&redirect_uri=" + redirectUri +
                 "&response_type=code" +
-                "&scope=openid%20profile%20email" +
+                "&scope=https://www.googleapis.com/auth/androidpublisher" +
                 "&access_type=offline" +
                 "&prompt=consent";
         return "redirect:" + authUri;
     }
 
     @GetMapping("/redirect")
-    public ResponseEntity<String> callback(@RequestParam("code") String code, Model model) {
+    private String oauth2Callback(String code) throws Exception {
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
@@ -55,39 +54,10 @@ public class GoogleController {
         map.add("grant_type", "authorization_code");
 
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity(tokenUri, request, String.class);
+        ResponseEntity<String> response = restTemplate.postForEntity("https://www.googleapis.com/oauth2/v4/token", request, String.class);
 
         System.out.println("response = " + response);
-        
-        String accessToken = extractAccessToken(response.getBody());
-        System.out.println("accessToken = " + accessToken);
 
-        String userInfo = getUserInfo(accessToken);
-
-        return ResponseEntity.ok(userInfo);
-    }
-
-    private String extractAccessToken(String responseBody) {
-        // JSON 파싱 라이브러리 사용 (예: Jackson, Gson 등)
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(responseBody);
-            return jsonNode.get("access_token").asText();
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private String getUserInfo(String accessToken) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(accessToken);
-
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<String> response = restTemplate.exchange(userInfoUri, HttpMethod.GET, entity, String.class);
-
-        return response.getBody();
+        return "";
     }
 }
